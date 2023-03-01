b85df835084fe4bd467f17d266dae02ef21ca502
Barbra <113640110+Barbra-Rotich@users.noreply.github.com>
2023-03-01 21:36:48 +0000

    Moved cli.py to junix folder

---
+++ notebooks/junix folder/cli.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from junix import junix
+import click
+
+@click.command()
+@click.option("-f", "--filepath", "filepath", required=True)
+@click.option("-o", "--output_dir", "output_dir", default=None)
+@click.option("-p", "--prefix", "prefix", default=None)
+def export_images(
+    filepath: str, output_dir: Optional[str] = None, prefix: Optional[str] = None
+):
+
+    junix.export_images(filepath=filepath, output_dir=output_dir, prefix=prefix)
+
+
+if __name__ == "__main__":
+    export_images()
