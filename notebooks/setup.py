ff0b9de8464aca27a1986f257376ffd31c93dd97
Barbra <113640110+Barbra-Rotich@users.noreply.github.com>
2023-03-01 21:22:54 +0000

    Add setup.py file

---
+++ notebooks/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup 
+setup(
+    name=”junix”, 
+    version=”0.1.4", 
+    author=”Damien Marlier”,
+    author_email=”damien.marlier@hotmail.fr”, 
+    description=”Utils to export images from Jupyter notebook”,
+    packages=[“junix”], 
+    entry_points={
+        “console_scripts”: [
+            “junix = junix.cli:export_images”
+        ]
+    }, 
+    install_requires=[ “click”, ],
+)
